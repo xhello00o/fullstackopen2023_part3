ae2008bdc757fda99c18610f0325f6375cd6ce9e
xhello00o <133972573+xhello00o@users.noreply.github.com>
2023-05-29 21:52:23 +0800

    part 3.19 to 3.22

--- part3/models/persons.js
+++ part3/models/persons.js
@@ -1,29 +1,39 @@
 
-const mongoose = require("mongoose")
+const mongoose = require('mongoose')
 
 
 
-const password = process.env.MONGO_DB_PW;
+const password = process.env.MONGO_DB_PW
 
 
-const url = `mongodb+srv://xuanhaotan:${password}@cluster0.btr3mlg.mongodb.net/?retryWrites=true&w=majority`;
+const url = `mongodb+srv://xuanhaotan:${password}@cluster0.btr3mlg.mongodb.net/?retryWrites=true&w=majority`
 
-mongoose.set("strictQuery", false);
-mongoose.connect(url);
+mongoose.set('strictQuery', false)
+mongoose.connect(url)
 
 const noteSchema = new mongoose.Schema({
-    name: String,
-    number: String,
-});
+    name: {
+        type:String,
+        minlength:3,
+        required: true},
+    number: {
+        type:String,
+        validate:{
+            validator: (v) =>{
+                return /\d{2,3}-\d+/.test(v)},
+            message:props => `${props.value} is not a valid phone number. It should contain XXX-XXXXX.`
+        },
+        minlength:8,
+        required: true} })
 
-const Person = mongoose.model("Person", noteSchema);
+const Person = mongoose.model('Person', noteSchema)
 
 noteSchema.set('toJSON', {
     transform: (document, returnedObject) => {
-      returnedObject.id = returnedObject._id.toString()
-      delete returnedObject._id
-      delete returnedObject.__v
+        returnedObject.id = returnedObject._id.toString()
+        delete returnedObject._id
+        delete returnedObject.__v
     }
-  })
-
+})
+  
 module.exports = Person
